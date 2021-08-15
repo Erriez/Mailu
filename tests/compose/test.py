4f07b47d43da6cad78afdd1c2264c7675eef6f71
Erriez <Erriez@users.noreply.github.com>
2021-08-15 20:05:44 +0200

    Apply code style and optimize if statements

--- tests/compose/test.py
+++ tests/compose/test.py
@@ -29,15 +29,15 @@ def sleep():
 
 def health_checks():
     exit_code = 0
-    #Iterating trough all containers dictionary
+    # Iterating trough all containers dictionary
     for container in client.containers(all=True):
-        #Perform "docker container inspect" on container based on container ID and save output to a dictionary
+        # Perform "docker container inspect" on container based on container ID and save output to a dictionary
         container_inspect = client.inspect_container(container['Id']) #Dict
 
         if "Health" in container_inspect['State'].keys():
             if container_inspect['State']['Health']['Status'] == "healthy":
                 print(Fore.GREEN + "Health status for " + container_inspect['Name'].replace("/", "") + " : " + Fore.CYAN + container_inspect['State']['Health']['Status'] + Style.RESET_ALL)
-            if container_inspect['State']['Health']['Status'] != "healthy":
+            else:
                 print(Fore.RED + "Container " + container_inspect['Name'].replace("/", "") + " is " + Fore.YELLOW + container_inspect['State']['Health']['Status']
                       + Fore.RED + ", FailingStreak: " + Fore.YELLOW + str(container_inspect['State']['Health']['FailingStreak'])
                       + Fore.RED + ", Log: " + Fore.YELLOW + str(container_inspect['State']['Health']['Log']) + Style.RESET_ALL)
@@ -45,17 +45,17 @@ def health_checks():
         else:
             if container_inspect['State']['Status'] == "running":
                 print(Fore.GREEN + "Running status for " + container_inspect['Name'].replace("/", "") + " : " + Fore.BLUE + container_inspect['State']['Status'] + Style.RESET_ALL)
-            if container_inspect['State']['Status'] != "running":
+            else:
                 print(Fore.RED + "Container " + container_inspect['Name'].replace("/", "") + " state is: " + Fore.YELLOW + container_inspect['State']['Status'] + Style.RESET_ALL)
                 exit_code = 1
 
-        #Saving Id, Name and state to a new dictionary
+        # Saving Id, Name and state to a new dictionary
         containers_dict = {}
         containers_dict['Name'] = container_inspect['Name'].replace("/", "")
         containers_dict['Id'] = container_inspect['Id']
         containers_dict['State'] = container_inspect['State']
 
-        #Adding the generated dictionary to a list
+        # Adding the generated dictionary to a list
         containers.append(containers_dict)
 
     if exit_code != 0:
@@ -63,13 +63,13 @@ def health_checks():
 
 def print_logs():
     print("Printing logs ...")
-    #Iterating through docker container inspect list and print logs
+    # Iterating through docker container inspect list and print logs
     for container in containers:
         print(Fore.LIGHTMAGENTA_EX + "Printing logs for: " + Fore.GREEN + container['Name'] + Style.RESET_ALL)
         sys.stdout.flush()
         print(subprocess.check_output('docker container logs ' + container['Name'], shell=True).decode())
 
-#Iterating over hooks in test folder and running them
+# Iterating over hooks in test folder and running them
 def hooks():
     print(Fore.LIGHTMAGENTA_EX + "Running hooks" + Style.RESET_ALL)
     for test_file in sorted(os.listdir(test_path)):
